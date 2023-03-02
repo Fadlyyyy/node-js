7b0790c552068874751f5095a1ba840d3256ba4b
Fadlyyyy <fadlyyansah86@gmail.com>
2023-03-02 09:00:15 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+// console.log('hello world!');
+
+
+
+// console.log(nama);
+// const nama = "fardli";
+// const cetakNama = (nama) => `hi nama saya ${nama}`;
+// console.log(cetakNama(nama));
+
+console.log(window);
